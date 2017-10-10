5e673305b5327462bdcf0312e528e792e0a27792
patriciochavez <patricio_chavez@hotmail.com>
2017-10-10 11:44:29 -0300

    first commit

--- Assets/Scripts/LoadWebCam.cs
+++ Assets/Scripts/LoadWebCam.cs
@@ -7,36 +7,43 @@ using System.IO;
 public class LoadWebCam : MonoBehaviour{
 	
 public RawImage webcam_image;
-private string screenShotURL= "http://IP:PORT/"; //change for your own public address
+private string screenShotURL= "http://200.5.235.51:8090/"; //change for your own public address
 
 private WebCamTexture WCTexture;
+private int tx_width = 320;
+private int tx_height = 240;
 
-// Use this for initialization
-IEnumerator Start () {
-	WCTexture = new WebCamTexture();
-	webcam_image.texture = WCTexture;
+    // Use this for initialization
+    IEnumerator Start () {
+    //WCTexture = new WebCamTexture(160, 120);
+    WCTexture = new WebCamTexture(tx_width, tx_height);
+    webcam_image.texture = WCTexture;
 	webcam_image.material.mainTexture = WCTexture;
-	WCTexture.Play();
+    WCTexture.Play();
 	
 		while (true) {			
 			yield return UploadPNG ();
-			yield return new WaitForSeconds (0.3f);
-		}
+            yield return new WaitForSeconds (0.1f);
+            //yield return new WaitForSeconds(1f);
+        }
 	}
 
 	IEnumerator UploadPNG()
-	{
+	{     
+
 		// We should only read the screen buffer after rendering is complete
 		yield return new WaitForEndOfFrame();
 
-		// Create a texture the size of the screen, RGB24 format
-		int width = Screen.width;
-		int height = Screen.height;
-		Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
-	
-		// Read screen contents into the texture
-		tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-		tex.Apply();
+        // Create a texture the size of the screen, RGB24 format
+        int width = Screen.width;
+        int height = Screen.height;
+        //Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+        Texture2D tex = new Texture2D(tx_width, tx_height, TextureFormat.RGB24, false);
+
+        // Read screen contents into the texture
+        //tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.ReadPixels(new Rect(width/2 - tx_width/2, height/2 - tx_height/2, tx_width - 2, tx_height - 2), 0, 0);
+        tex.Apply();
 
 		// Encode texture into PNG
 		//byte[] bytes = tex.EncodeToPNG();
